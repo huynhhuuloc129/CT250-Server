683d002e2f3ddcf8b21dc87cb6c8506e1611f94e
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-10-17 10:05:02 +0700

    add public api

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -4,15 +4,15 @@ import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateAdminDto } from './dto/create-admin.dto';
 import { USER_ROLE, User } from '../users/entities/user.entity';
 import { RequiredRoles } from '../auth/decorators/required-roles.decorator';
+import { Public } from '../auth/utils';
 
 @Controller('admin')
 @ApiTags('admin')
-@ApiBearerAuth('bearer')
 export class AdminController {
 	constructor(private readonly adminService: AdminService) {}
 
 	@Post()
-	@RequiredRoles(USER_ROLE.ADMIN)
+	@Public()
 	@ApiOperation({ summary: 'Create one admin user' })
 	async createOne(@Body() createDto: CreateAdminDto): Promise<User> {
 		return await this.adminService.createOneAdmin(createDto);
