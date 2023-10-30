d7b602c60f7e84186fee4315ab354e4d9b595172
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-10-31 01:16:51 +0700

    init(controller, service, dto): initialize

--- src/modules/utility/utility.controller.ts
+++ src/modules/utility/utility.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Patch } from '@nestjs/common';
 import { UtilityService } from './utility.service';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 @Controller('utility')
+@ApiTags('utility')
+@ApiBearerAuth()
 export class UtilityController {
-  constructor(private readonly utilityService: UtilityService) {}
+	constructor(private readonly utilityService: UtilityService) {}
+	@Get()
+	findAll() {}
+	@Get(':utilityId')
+	findOne() {}
+	@Patch(':utilityId')
+	updateOne() {}
+	@Delete(':utilityId')
+	deleteOne() {}
 }
--- src/modules/utility/utility.service.ts
+++ src/modules/utility/utility.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Utility } from './entities/utility.entity';
+import { UpdatePhoto } from '../photo/type/update-photo.type';
+import { UpdateUtilityDto } from './dto/update-utility.dto';
 
 @Injectable()
-export class UtilityService {}
+export class UtilityService {
+	constructor(
+		@InjectRepository(Utility) private utilitiesRepository: Repository<Utility>,
+	) {}
+
+	async updateOneByCondititon(
+		filter: object | object[],
+		updateDto: UpdateUtilityDto | UpdatePhoto,
+	): Promise<Utility> {
+		const entity = await this.utilitiesRepository.findOne({ where: filter });
+
+		if (!entity) {
+			throw new NotFoundException(`Not found utility`);
+		}
+
+		return await this.utilitiesRepository.save({
+			...entity,
+			...updateDto,
+		});
+	}
+
+	async findOneByCondititon(filter: object | object[]): Promise<Utility> {
+		return await this.utilitiesRepository.findOne({
+			where: filter,
+			relations: {
+				photo: true,
+			},
+		});
+	}
+	findMany() {}
+	deleteOne() {}
+}
